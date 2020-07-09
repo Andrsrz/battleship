7ec89ca52f16752fce9f6df98f2f507da69cc8b3
Andres Ruiz <andrsruiz@gmail.com>
2020-07-09 16:19:59 -0500

    Checking first Ship

--- src/classes/gameboard.js
+++ src/classes/gameboard.js
@@ -1,5 +1,10 @@
+import { Ship } from './ship';
+
 class Gameboard{
 	constructor(){
+		this.aircraft = new Ship('Aircraft Carrier', 5);
+		this.ships = [];
+		this.ships.push(this.aircraft);
 	}
 }
 
