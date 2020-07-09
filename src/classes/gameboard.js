ac49e1f5df8c662f5d6dfeb25390bf848bad369b
Andres Ruiz <andrsruiz@gmail.com>
2020-07-09 16:27:49 -0500

    Checking for Destroyer Ships

--- src/classes/gameboard.js
+++ src/classes/gameboard.js
@@ -5,10 +5,14 @@ class Gameboard{
 		this.aircraft = new Ship('Aircraft Carrier', 5);
 		this.battleship = new Ship('Battleship', 4);
 		this.cruiser = new Ship('Cruiser', 3);
+		this.destroyer1 = new Ship('Destroyer', 2);
+		this.destroyer2 = new Ship('Destroyer', 2);
 		this.ships = [];
 		this.ships.push(this.aircraft);
 		this.ships.push(this.battleship);
 		this.ships.push(this.cruiser);
+		this.ships.push(this.destroyer1);
+		this.ships.push(this.destroyer2);
 	}
 }
 
--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -6,6 +6,7 @@ describe("Create Gameboard", () => {
 	const AircraftCarrier = new Ship('Aircraft Carrier', 5);
 	const Battleship = new Ship('Battleship', 4);
 	const Cruiser = new Ship('Cruiser', 3);
+	const Destroyer = new Ship('Destroyer', 2);
 
 	test("Gameboard has an Aircraft Carrier", () => {
 		expect(AircraftCarrier).toMatchObject(myGameboard.ships[0]);
@@ -17,5 +18,10 @@ describe("Create Gameboard", () => {
 
 	test("Gameboard has a Cruiser", () => {
 		expect(Cruiser).toMatchObject(myGameboard.ships[2]);
+	});
+
+	test("Gameboard has two Destroyers", () => {
+		expect(Destroyer).toMatchObject(myGameboard.ships[3]);
+		expect(Destroyer).toMatchObject(myGameboard.ships[4]);
 	});
 });
