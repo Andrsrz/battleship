9218fa2f3a3a693eb161ba33d3e5e2c8cf55ed67
Andres Ruiz <andrsruiz@gmail.com>
2020-07-09 16:24:19 -0500

    Check for Cruiser Ship

--- src/classes/gameboard.js
+++ src/classes/gameboard.js
@@ -4,9 +4,11 @@ class Gameboard{
 	constructor(){
 		this.aircraft = new Ship('Aircraft Carrier', 5);
 		this.battleship = new Ship('Battleship', 4);
+		this.cruiser = new Ship('Cruiser', 3);
 		this.ships = [];
 		this.ships.push(this.aircraft);
 		this.ships.push(this.battleship);
+		this.ships.push(this.cruiser);
 	}
 }
 
--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -5,6 +5,7 @@ describe("Create Gameboard", () => {
 	let myGameboard = new Gameboard();
 	const AircraftCarrier = new Ship('Aircraft Carrier', 5);
 	const Battleship = new Ship('Battleship', 4);
+	const Cruiser = new Ship('Cruiser', 3);
 
 	test("Gameboard has an Aircraft Carrier", () => {
 		expect(AircraftCarrier).toMatchObject(myGameboard.ships[0]);
@@ -12,5 +13,9 @@ describe("Create Gameboard", () => {
 
 	test("Gameboard has a Battleship", () => {
 		expect(Battleship).toMatchObject(myGameboard.ships[1]);
+	});
+
+	test("Gameboard has a Cruiser", () => {
+		expect(Cruiser).toMatchObject(myGameboard.ships[2]);
 	});
 });
