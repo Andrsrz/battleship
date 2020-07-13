1f31926dae200be974d43a4374d74c69cd74f930
Andres Ruiz <andrsruiz@gmail.com>
2020-07-13 16:17:20 -0500

    Test vue component

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
 	<div id="app">
 		<img alt="Vue logo" src="./assets/logo.png">
 		<HelloWorld msg="Welcome to Your Vue.js App"/>
+		<FancyHeading title="Hello, Heading" color="blue"/>
 	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import FancyHeading from './components/FancyHeading.vue';
 
 export default {
 	name: 'App',
 	components: {
-		HelloWorld
+		HelloWorld,
+		FancyHeading
 	}
 }
 </script>
